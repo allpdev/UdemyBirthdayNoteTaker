dcc5f0c1c5a8704ec1a124ce27ea92bc73b5f69d
allpdev <alpkctpdev@gmail.com>
2022-10-27 20:09:19 +0300

    Connections

--- UdemyBirthdayNote/ViewController.swift
+++ UdemyBirthdayNote/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var birthdayText: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    
 
 }
 
