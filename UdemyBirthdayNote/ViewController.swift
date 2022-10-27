8f94d3350a1d46a44cb3f9affb5feb4e19e9a7b5
allpdev <alpkctpdev@gmail.com>
2022-10-27 20:34:48 +0300

    DataStorage & Casting

--- UdemyBirthdayNote/ViewController.swift
+++ UdemyBirthdayNote/ViewController.swift
@@ -19,10 +19,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        //Casting -> as? & as!
+        if let newName = storedName as? String {
+            nameLabel.text = newName
+        }
+        
+        if let newbirthday = storedBirthday as? String {
+            birthdayLabel.text = newbirthday
+        }
+        
     }
     
     //Button func
     @IBAction func saveButton(_ sender: Any) {
+        //Data Storage
+        UserDefaults.standard.set(nameText.text!, forKey: "name")
+        UserDefaults.standard.set(birthdayText.text!, forKey: "birthday")
+        //UserDefaults.standard.synchronize()
         
         nameLabel.text = "Name : \(nameText.text!)"
         birthdayLabel.text = "Birthday : \(birthdayText.text!)"
