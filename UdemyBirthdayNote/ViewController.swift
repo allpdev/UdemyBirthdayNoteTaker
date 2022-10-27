25d0290a723b86350774c4dccc0fe41a7f332f10
allpdev <alpkctpdev@gmail.com>
2022-10-27 20:21:28 +0300

    Button Func

--- UdemyBirthdayNote/ViewController.swift
+++ UdemyBirthdayNote/ViewController.swift
@@ -9,20 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //Connections
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var birthdayText: UITextField!
     @IBOutlet weak var nameLabel: UILabel!
-    
     @IBOutlet weak var birthdayLabel: UILabel!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
     
-    
+    //Button func
     @IBAction func saveButton(_ sender: Any) {
+        
+        nameLabel.text = "Name : \(nameText.text!)"
+        birthdayLabel.text = "Birthday : \(birthdayText.text!)"
+        
     }
     
 
